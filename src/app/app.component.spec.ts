00958ab0d0a1109197db6567c3cb5b2bafd49641
arthur.jacquemin <arthurjacquemin2@gmail.com>
2023-09-20 17:49:36 +0200

    test: app

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,48 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { MessageService } from 'primeng/api';
+import { AppModule } from './app.module';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
-  }));
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
+  let messageService: MessageService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [AppComponent],
+      imports: [AppModule],
+      providers: [MessageService],
+    });
+
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
+    messageService = TestBed.inject(MessageService);
+  });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'frontend'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('frontend');
-  });
+  it('should upload an excel file', () => {
+    spyOn(messageService, 'add');
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('frontend app is running!');
+    const mockEvent: any = {
+      files: [
+        {
+          name: 'test.xlsx',
+          size: 1234,
+          type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        },
+      ],
+    };
+
+    app.onUpload(mockEvent);
+
+    expect(messageService.add).toHaveBeenCalledWith({
+      severity: 'info',
+      summary: 'Success',
+      detail: 'File Uploaded and data stored in database',
+    });
   });
 });
